c1e8128c85ee4df740f189c3ed6008677393f124
pc <1683672460@qq.com>
2019-06-28 11:13:58 +0800

    Blacklist修改客户

--- src/main/java/cn/tcmp/FinanceApplication.java
+++ src/main/java/cn/tcmp/FinanceApplication.java
@@ -3,7 +3,7 @@ package cn.tcmp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@MapperScan("cn.tcmp.*.dao")
+
 @SpringBootApplication
 @MapperScan("cn.tcmp.*.dao")
 public class FinanceApplication {
