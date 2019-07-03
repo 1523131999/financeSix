f7358befafc6a33825e2e01ae06b36facbd78912
Lenovo <2576380450@qq.com>
2019-07-03 10:16:21 +0800

    Merge branch 'master' of https://github.com/1523131999/financeSix
    # Conflicts:
    #	src/main/java/cn/tcmp/entity/Client.java
    #	src/main/resources/application.properties

--- src/main/java/cn/tcmp/tiantian/controller/TTClientController.java
+++ src/main/java/cn/tcmp/tiantian/controller/TTClientController.java
@@ -2,16 +2,23 @@ package cn.tcmp.tiantian.controller;
 
 import cn.tcmp.entity.Client;
 import cn.tcmp.tiantian.service.TTClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
+@RequestMapping("ttClientController")
 public class TTClientController {
 
 
+    @Autowired
     private TTClientService service;
 
-
+    @RequestMapping("addClient")
     public String addClient(Client client){
         service.addClient(client);
         return "/tiantian/client-add";
     }
+
+
 }
--- src/main/java/cn/tcmp/ymy/service/ClientService.java
+++ src/main/java/cn/tcmp/ymy/service/ClientService.java
@@ -3,9 +3,10 @@ package cn.tcmp.ymy.service;
 import cn.tcmp.entity.Client;
 import com.github.pagehelper.PageInfo;
 
+
 import java.util.List;
 
 public interface ClientService {
-    PageInfo<Client> queryAll(Client client,Integer pageSize,Integer pageNum,String startTime,String endTime);
+    PageInfo<Client> queryAll(Client client, Integer pageSize, Integer pageNum, String startTime, String endTime);
     Integer queryClientByFour(Client client);
 }
