da599e0f3e0ca6be473b993796a30fa06b18b55b
ymy <850618511@qq.com>
2019-07-03 16:37:52 +0800

    jkl

---
+++ src/test/java/cn/tcmp/ymy/service/ClientServiceImplTest.java
@@ -0,0 +1,30 @@
+package cn.tcmp.ymy.service;
+
+import cn.tcmp.entity.Cert;
+import cn.tcmp.entity.Client;
+import cn.tcmp.entity.Secondarymanager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ClientServiceImplTest {
+        @Autowired
+        private ClientService service;
+    @Test
+    public void queryCiYao() {
+        Secondarymanager secondarymanager=new Secondarymanager();
+        Cert cert=new Cert();
+        Client client=new Client();
+
+        secondarymanager.setClient(client);
+
+    }
+}
